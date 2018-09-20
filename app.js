2eca1775b0982170808cdd49af389a43c125137c
Elminson De Oleo Baez <edeoleo@gmail.com>
2018-09-19 22:40:49 -0400

    Handling fs, os, path modules

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const logger = require('./logger');
 const path = require('path');
-logger.log("example");
+const os = require('os');
+const fs = require('fs');
+//logger.log("example");
+///var pathObje = path.parse(__filename);
+//console.log(pathObje);
 
-var pathObje = path.parse(__filename);
-console.log(pathObje);
+// var totalm = os.totalmem();
+// var freemem = os.freemem();
+// console.log("Total totalmtotalm " + totalm + "\n");
+// console.log("Total freemem " + freemem + "\n");
+// console.log(`Total freemem ${totalm} `);
+// console.log(`Total freemem ${freemem} `);
+
+// const files = fs.readdirSync('./');
+// console.log(files);
+
+fs.readdir('./', function (err, files) {
+    if (err) console.log('Error', err);
+    else console.log(files);
+});
